0be981adccb048edfcae8064ba67b5107484f4c6
bob <bob_dw@outlook.com>
2025-03-28 15:20:13 +0800

    feat:模型配置修改

--- src/helpers/getAiModel.tsx
+++ src/helpers/getAiModel.tsx
@@ -3,13 +3,13 @@ import {globalConfig} from "../config/global";
 
 export type AiType = "pro" | "vision";
 
-const { keys, api } = globalConfig;
+const { modal, api } = globalConfig;
 export const getAiModel = (
     ai: GoogleGenerativeAI,
     type: AiType = "pro",
     options?: BaseParams,
 ) => ai.getGenerativeModel({
     ...options,
-    model: type === "pro" ? "gemini-2.0-flash" : "gemini-2.0-flash",
+    model: modal ? modal : "gemini-2.0-flash",
 }, {baseUrl: api});
 
